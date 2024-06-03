c4733394eb3fe42fb062896c8aa640e9c664a8a9
Saketh Nimmagadda <45970393+saketh-n@users.noreply.github.com>
2024-06-03 10:53:04 -0700

    Added functionality for adding, retrieving and deleting items

--- index.js
+++ index.js
@@ -2,8 +2,37 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+// Create an in-memory data store and allow us to add items via api
+let items = [];
+
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+app.post("/items", (req, res) => {
+  const newItem = req.body;
+  items.push(newItem);
+  res.status(201).json(newItem);
+});
+
+// API to retrieve Items, by an id
+app.get("/items/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const item = items.find((i) => i.id === id);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).send("Item not found");
+  }
+});
+
+// Delete an item by ID
+app.delete("/items/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  items = items.filter((i) => i.id !== id);
+  res.status(204).send();
 });
 
 app.listen(port, () => {
